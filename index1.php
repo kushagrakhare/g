12f70114a09770472fb76bfa0499d980e19ca511
kushagrakhare <kushagrakhare1991@gmail.com>
2016-04-06 16:06:13 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -9,13 +9,13 @@
 		"ProductMRP": "gggg",
 		"ProductBBPrice": "hhhh"
 	}, {
-		"ProductCode": "aaaa",
-		"ProductName": "bbbbb",
-		"ProductGrammage": "ccccc",
-		"ProductBarcode": "dddd",
-		"ProductCatCode": "eeee",
-		"ProductSubCode": "ffff",
-		"ProductMRP": "gggg",
-		"ProductBBPrice": "hhhh"
+		"ProductCode": "delhi",
+		"ProductName": "delhi",
+		"ProductGrammage": "delhi",
+		"ProductBarcode": "delhi",
+		"ProductCatCode": "delhi",
+		"ProductSubCode": "delhi",
+		"ProductMRP": "delhi",
+		"ProductBBPrice": "delhi"
 	}]
 }
