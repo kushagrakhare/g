20009f76d67ba99eafa9fdf673aec2072d2a1b4a
kushagrakhare <kushagrakhare1991@gmail.com>
2016-04-06 16:51:23 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -9,6 +9,33 @@
 		"ProductMRP": "40",
 		"ProductBBPrice": "35"
 	}, {
+		"ProductCode": "aaaa",
+		"ProductName": "Tomato small",
+		"ProductGrammage": "ccccc",
+		"ProductBarcode": "Tomato",
+		"ProductCatCode": "eeee",
+		"ProductSubCode": "ffff",
+		"ProductMRP": "38",
+		"ProductBBPrice": "35"
+	}, {
+		"ProductCode": "aaaa",
+		"ProductName": "Tomato imported",
+		"ProductGrammage": "ccccc",
+		"ProductBarcode": "Tomato",
+		"ProductCatCode": "eeee",
+		"ProductSubCode": "ffff",
+		"ProductMRP": "47",
+		"ProductBBPrice": "32"
+	}, {
+		"ProductCode": "aaaa",
+		"ProductName": "Horliks",
+		"ProductGrammage": "ccccc",
+		"ProductBarcode": "Tomato",
+		"ProductCatCode": "eeee",
+		"ProductSubCode": "ffff",
+		"ProductMRP": "40",
+		"ProductBBPrice": "35"
+	}, {
 		"ProductCode": "delhi",
 		"ProductName": "Rice",
 		"ProductGrammage": "delhi",
