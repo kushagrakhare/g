85a7fa52158ee682ca6eb720bead6326bfb2bd04
kushagrakhare <kushagrakhare1991@gmail.com>
2016-04-07 10:43:16 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -4,8 +4,8 @@
 		"ProductName": "Tomato",
 		"ProductGrammage": "ccccc",
 		"ProductBarcode": "Tomato",
-		"ProductCatCode": "1",
-		"ProductSubCode": "ffff",
+		"ProductCatCode": "7",
+		"ProductSubCode": "77",
 		"ProductMRP": "40",
 		"ProductBBPrice": "35"
 	}, {
