784ee54f3a41e43706e1f329280d565c330438ae
kushagrakhare <kushagrakhare1991@gmail.com>
2016-04-08 14:22:59 +0530

    Added files via upload

---
+++ sub.php
@@ -0,0 +1,21 @@
+{
+	"ProductListByCat": [{
+		"ProductCode": "Delhi",
+		"ProductName": "Delhi",
+		"ProductGrammage": "Delhi",
+		"ProductBarcode": "Delhi",
+		"ProductCatCode": "Delhi",
+		"ProductSubCode": "Delhi",
+		"ProductMRP": "Delhi",
+		"ProductBBPrice": "Delhi"
+	}, {
+		"ProductCode": "Delhi",
+		"ProductName": "horliks",
+		"ProductGrammage": "Delhi",
+		"ProductBarcode": "Delhi",
+		"ProductCatCode": "Delhi",
+		"ProductSubCode": "Delhi",
+		"ProductMRP": "Delhi",
+		"ProductBBPrice": "Delhi"
+	}]
+}
