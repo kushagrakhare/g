2ed20d68b90eff2db1df39150637a135653e0080
kushagrakhare <kushagrakhare1991@gmail.com>
2016-04-15 16:19:56 +0530

    Update xmlhai.php

--- xmlhai.php
+++ xmlhai.php
@@ -2,7 +2,7 @@
 <song>
 <id>1</id>
 <title>Someone Like You</title>
-<artist>Adele</artist>
+<artist>Adelfe</artist>
 <duration>4:47</duration>
 <thumb_url>http://api.androidhive.info/music/images/adele.png</thumb_url>
 </song>
