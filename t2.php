b64390007421b4b4ebaecac76b754c5b5abcc08c
kushagrakhare <kushagrakhare1991@gmail.com>
2017-01-31 16:25:52 +0530

    Update t2.php

--- t2.php
+++ t2.php
@@ -1,37 +1,46 @@
 [{
 	"id": "1",
-	"image_title": "aaaaa",
-	"image_url": "http:\/\/androidblog.esy.es\/images\/cupcake-1.png"
+	"image_title": "History",
+	"image_time": "2:00 AM",
+	"image_url": "http://collegeholkar.org/upload/Dr_Pradeep_Sharma.png"
 }, {
 	"id": "2",
-	"image_title": "ddddd",
-	"image_url": "http:\/\/androidblog.esy.es\/images\/donut-2.png"
+	"image_title": "Geography",
+	"image_time": "3:00 AM",
+	"image_url": "http://collegeholkar.org/upload/Shivlal_Mewada1.jpg"
 }, {
 	"id": "3",
-	"image_title": "ssdsdr",
-	"image_url": "http:\/\/androidblog.esy.es\/images\/eclair-3.png"
+	"image_title": "Physics",
+	"image_time": "3:30 AM",
+	"image_url": "http://collegeholkar.org/upload/BeautyPlus_20151119130732_save.jpg"
 }, {
 	"id": "4",
-	"image_title": "dgdfgfd",
+	"image_title": "Chemistry",
+	"image_time": "4:00 AM",
 	"image_url": "http:\/\/androidblog.esy.es\/images\/froyo-4.png"
 }, {
 	"id": "5",
-	"image_title": "Android 2.3 GingerBread",
+	"image_title": "Maths",
+	"image_time": "5:00 AM",
 	"image_url": "http:\/\/androidblog.esy.es\/images\/gingerbread-5.png"
 }, {
 	"id": "6",
-	"image_title": "Android 3.0 HoneyComb",
-	"image_url": "http:\/\/androidblog.esy.es\/images\/honeycomb-6.png"
+	"image_title": "Computer",
+	"image_time": "5:50 AM",
+	"image_url": "http://collegeholkar.org/upload/970672_704708689606666_7900309174862126741_n.jpg"
 }, {
 	"id": "7",
-	"image_title": "Android 4.0 Ice Cream Sandwich",
-	"image_url": "http:\/\/androidblog.esy.es\/images\/ice-cream-sandwich-7.png"
+	"image_title": "Economics",
+	"image_time": "6:00 AM",
+	"image_url": "http://collegeholkar.org/upload/IMG-20151114-WA0010.jpg"
 }, {
 	"id": "8",
-	"image_title": "Android 4.1 Jelly Bean",
-	"image_url": "http:\/\/androidblog.esy.es\/images\/jellybean-8.png"
+	"image_title": "commerse",
+	"image_time": "6:00 AM",
+	"image_url": "http://collegeholkar.org/upload/manish.png"
 }, {
 	"id": "9",
-	"image_title": "Android 4.4 KitKat",
-	"image_url": "http:\/\/androidblog.esy.es\/images\/kitkat-9.png"
+	"image_title": "Graph",
+	"image_time": "6:50 AM",
+	"image_url": "http://collegeholkar.org/upload/1800296_520749021394746_906416541762378070_n.jpg"
 }]
