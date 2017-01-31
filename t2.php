3318df25a88ee378f798d5552cbea6bd5872c9fd
kushagrakhare <kushagrakhare1991@gmail.com>
2017-01-31 13:27:18 +0530

    Update t2.php

--- t2.php
+++ t2.php
@@ -1,28 +1,37 @@
-{
-   "result":[
-      {
-         "username":"faiz",
-         "name":"Faiz Khan",
-         "course":"BCA",
-         "session":"2014-2017"
-      },
-      {
-         "username":"belal",
-         "name":"Belal Khan",
-         "course":"MCA",
-         "session":"2015-2018"
-      },
-      {
-         "username":"ramiz",
-         "name":"Ramiz Khan",
-         "course":"MBA",
-         "session":"2015-2017"
-      },
-      {
-         "username":"zafar",
-         "name":"Zafar Khan",
-         "course":"MBA",
-         "session":"2014-2016"
-      }
-   ]
-}
+[{
+	"id": "1",
+	"image_title": "aaaaa",
+	"image_url": "http:\/\/androidblog.esy.es\/images\/cupcake-1.png"
+}, {
+	"id": "2",
+	"image_title": "ddddd",
+	"image_url": "http:\/\/androidblog.esy.es\/images\/donut-2.png"
+}, {
+	"id": "3",
+	"image_title": "ssdsdr",
+	"image_url": "http:\/\/androidblog.esy.es\/images\/eclair-3.png"
+}, {
+	"id": "4",
+	"image_title": "dgdfgfd",
+	"image_url": "http:\/\/androidblog.esy.es\/images\/froyo-4.png"
+}, {
+	"id": "5",
+	"image_title": "Android 2.3 GingerBread",
+	"image_url": "http:\/\/androidblog.esy.es\/images\/gingerbread-5.png"
+}, {
+	"id": "6",
+	"image_title": "Android 3.0 HoneyComb",
+	"image_url": "http:\/\/androidblog.esy.es\/images\/honeycomb-6.png"
+}, {
+	"id": "7",
+	"image_title": "Android 4.0 Ice Cream Sandwich",
+	"image_url": "http:\/\/androidblog.esy.es\/images\/ice-cream-sandwich-7.png"
+}, {
+	"id": "8",
+	"image_title": "Android 4.1 Jelly Bean",
+	"image_url": "http:\/\/androidblog.esy.es\/images\/jellybean-8.png"
+}, {
+	"id": "9",
+	"image_title": "Android 4.4 KitKat",
+	"image_url": "http:\/\/androidblog.esy.es\/images\/kitkat-9.png"
+}]
