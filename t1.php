208f5569eaf2ce536e707566442982738e835743
kushagrakhare <kushagrakhare1991@gmail.com>
2017-01-31 15:48:04 +0530

    Update t1.php

--- t1.php
+++ t1.php
@@ -1,37 +1,46 @@
 [{
 	"id": "1",
 	"image_title": "History",
+	"image_time": "2:00 AM",
 	"image_url": "http://collegeholkar.org/upload/Dr_Pradeep_Sharma.png"
 }, {
 	"id": "2",
 	"image_title": "Geography",
+	"image_time": "3:00 AM",
 	"image_url": "http://collegeholkar.org/upload/Shivlal_Mewada1.jpg"
 }, {
 	"id": "3",
 	"image_title": "Physics",
+	"image_time": "3:30 AM",
 	"image_url": "http://collegeholkar.org/upload/BeautyPlus_20151119130732_save.jpg"
 }, {
 	"id": "4",
 	"image_title": "Chemistry",
+	"image_time": "4:00 AM",
 	"image_url": "http:\/\/androidblog.esy.es\/images\/froyo-4.png"
 }, {
 	"id": "5",
 	"image_title": "Maths",
+	"image_time": "5:00 AM",
 	"image_url": "http:\/\/androidblog.esy.es\/images\/gingerbread-5.png"
 }, {
 	"id": "6",
 	"image_title": "Computer",
+	"image_time": "5:50 AM",
 	"image_url": "http://collegeholkar.org/upload/970672_704708689606666_7900309174862126741_n.jpg"
 }, {
 	"id": "7",
 	"image_title": "Economics",
+	"image_time": "6:00 AM",
 	"image_url": "http://collegeholkar.org/upload/IMG-20151114-WA0010.jpg"
 }, {
 	"id": "8",
 	"image_title": "commerse",
+	"image_time": "6:00 AM",
 	"image_url": "http://collegeholkar.org/upload/manish.png"
 }, {
 	"id": "9",
 	"image_title": "Graph",
+	"image_time": "6:50 AM",
 	"image_url": "http://collegeholkar.org/upload/1800296_520749021394746_906416541762378070_n.jpg"
 }]
