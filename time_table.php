2c1981cadd26ef6230780e5c59d7b0b6f9fea308
kushagrakhare <kushagrakhare1991@gmail.com>
2017-01-31 11:49:46 +0530

    Add files via upload

---
+++ time_table.php
@@ -0,0 +1,28 @@
+{
+   "result":[
+      {
+         "username":"faiz",
+         "name":"Faiz Khan",
+         "course":"BCA",
+         "session":"2014-2017"
+      },
+      {
+         "username":"belal",
+         "name":"Belal Khan",
+         "course":"MCA",
+         "session":"2015-2018"
+      },
+      {
+         "username":"ramiz",
+         "name":"Ramiz Khan",
+         "course":"MBA",
+         "session":"2015-2017"
+      },
+      {
+         "username":"zafar",
+         "name":"Zafar Khan",
+         "course":"MBA",
+         "session":"2014-2016"
+      }
+   ]
+}
