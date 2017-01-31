b67865dee3efcdf5b272bf68de6ddc7f0160c668
kushagrakhare <kushagrakhare1991@gmail.com>
2017-01-31 15:17:56 +0530

    Update t1.php

--- t1.php
+++ t1.php
@@ -1,37 +1,37 @@
 [{
 	"id": "1",
-	"image_title": "Android 1.5 Cupcake",
+	"image_title": "History",
 	"image_url": "http:\/\/androidblog.esy.es\/images\/cupcake-1.png"
 }, {
 	"id": "2",
-	"image_title": "Android 1.6 Donut",
+	"image_title": "Geography",
 	"image_url": "http:\/\/androidblog.esy.es\/images\/donut-2.png"
 }, {
 	"id": "3",
-	"image_title": "Android 2.0 Eclair",
+	"image_title": "Physics",
 	"image_url": "http:\/\/androidblog.esy.es\/images\/eclair-3.png"
 }, {
 	"id": "4",
-	"image_title": "Android 2.2 Froyo",
+	"image_title": "Chemistry",
 	"image_url": "http:\/\/androidblog.esy.es\/images\/froyo-4.png"
 }, {
 	"id": "5",
-	"image_title": "Android 2.3 GingerBread",
+	"image_title": "Maths",
 	"image_url": "http:\/\/androidblog.esy.es\/images\/gingerbread-5.png"
 }, {
 	"id": "6",
-	"image_title": "Android 3.0 HoneyComb",
+	"image_title": "Computer",
 	"image_url": "http:\/\/androidblog.esy.es\/images\/honeycomb-6.png"
 }, {
 	"id": "7",
-	"image_title": "Android 4.0 Ice Cream Sandwich",
+	"image_title": "Economics",
 	"image_url": "http:\/\/androidblog.esy.es\/images\/ice-cream-sandwich-7.png"
 }, {
 	"id": "8",
-	"image_title": "Android 4.1 Jelly Bean",
+	"image_title": "commerse",
 	"image_url": "http:\/\/androidblog.esy.es\/images\/jellybean-8.png"
 }, {
 	"id": "9",
-	"image_title": "Android 4.4 KitKat",
+	"image_title": "Graph",
 	"image_url": "http:\/\/androidblog.esy.es\/images\/kitkat-9.png"
 }]
